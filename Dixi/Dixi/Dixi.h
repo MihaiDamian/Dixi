f446dfda18cca05d42a9fae17579cb2007718d8d
Mihai Damian <damianmihairadu@gmail.com>
2015-02-28 17:10:25 +0200

    Created Xcode project

---
+++ Dixi/Dixi/Dixi.h
@@ -0,0 +1,19 @@
+//
+//  Dixi.h
+//  Dixi
+//
+//  Created by Mihai Damian on 28/02/15.
+//  Copyright (c) 2015 Mihai Damian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Dixi.
+FOUNDATION_EXPORT double DixiVersionNumber;
+
+//! Project version string for Dixi.
+FOUNDATION_EXPORT const unsigned char DixiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Dixi/PublicHeader.h>
+
+
