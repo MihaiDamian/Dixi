cf7d4ccbb2f09a0ec037d33c51c755f4373b6f23
Mihai Damian <damianmihairadu@gmail.com>
2015-03-21 12:29:32 +0200

    Added example for declaring multiple constraints

--- Dixi/Demo.playground/contents.swift
+++ Dixi/Demo.playground/contents.swift
@@ -5,16 +5,24 @@ import UIKit
 
 let view1 = UIView()
 let view2 = UIView()
+let view3 = UIView()
 let superview = UIView()
 superview.addSubview(view1)
+superview.addSubview(view2)
+superview.addSubview(view3)
 
 
 // Dixi operators are designed to closely imitate Apple's visual format syntax. The syntax is not identical, though.
-// Unlike the visual format syntax, you can't create multiple constraints form a single Dixi expression.
 let standardHorizontalSpacingConstraint = view1 - view2
+
 // Dixi represents constraints as a custom opaque type. You can transform it to a NSLayoutConstraint or use the UIView
 // extension convenience methods to add them directly to a view as NSLayoutConstraints.
 superview.addConstraint(standardHorizontalSpacingConstraint)
+
+// Unlike the visual format syntax, you can't create multiple constraints form a single Dixi expression.
+// Instead you can easily create an array of constraints.
+let constraints = [view1 - view2, view2 - view3]
+superview.addConstraints(constraints)
 
 let customHorizontalDistanceConstraint = view1 |- 10 -| view2
 let flushViewsConstraint = view1 || view2
